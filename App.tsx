72613c257aacfcbc7b34534a5430d1933078df63
Lucas Amorim <lucas.ad@hotmail.com>
2021-07-07 21:46:54 -0300

    Projeto react native criado no NLW

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_700Bold, Rajdhani_500Medium } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+import { Routes } from './src/routes';
+import { Background } from './src/components/background'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </Background>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Gameplay
+Aplicação sendo desenvolvida no evento da <a href="https://instagram.com/rocketseat_oficial">Rocketseat</a> o NLW (Next Level Week).
+Sendo desenvolvido em React Native e terá integração com várias API's.
