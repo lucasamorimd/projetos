677eb83ced9f47b515eb1dc3a99b314cf8983fa1
Lucas Amorim <lucas.ad@hotmail.com>
2021-07-30 21:44:03 -0300

    Estilizando email enviado para confirmação

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 ## Sistema de agendamento para clínica
 <a href="https://clinicasite.lad566.com.br">Confira lá a demonstração</a>
 Sistema projetado para:
+
 ## Usuário
-- Usuário se cadastra
-- Agenda serviço (Consulta, Exame ou Procedimento)
-- Visualiza Agendamentos
+- Usuário se cadastra.
+- Confirma o Email.
+- Agenda serviço (Consulta, Exame ou Procedimento).
+- Visualiza Agendamentos.
+- Recebe o resultado.
 
 ## Clínica
-- Registra as Unidades
-- Registra os médicos de cada unidade
-- Registra Serviços disponíveis em cada Unidade
-- Registra os médicos que realizam cada tipo de serviço
+- Registra as Unidades.
+- Registra os médicos de cada unidade.
+- Registra Serviços disponíveis em cada Unidade.
+- Registra os médicos que realizam cada tipo de serviço.
 
--- src/controllers/ConfirmController.php
+++ src/controllers/ConfirmController.php
@@ -23,9 +23,10 @@ class ConfirmController extends Controller
         $dados_usuario = Usuario::select()->where(new F('MD5', 'id_usuario'), $id['id'])->one();
         $link = 'https://clinicasite.lad566.com.br/usuarios/confirmar/' . $id['id'];
         $assunto = "Confirmação de conta";
-        $msg = "Olá " . $dados_usuario['nome'] . ", Seja bem-vindo(a). Clique no link abaixo para confirmar e acessar sua conta.\n\n" . $link;
-
-        $encoding = "utf-8";
+        $msg = $this->createEmail([
+            'nome' => $dados_usuario['nome'],
+            'link' => $link
+        ]);        $encoding = "utf-8";
         // Preferences for Subject field
         $subject_preferences = array(
             "input-charset" => $encoding,
@@ -35,7 +36,7 @@ class ConfirmController extends Controller
         );
 
         // Mail header
-        $headers = "Content-type: text/plain; charset=" . $encoding . " \r\n";
+        $headers = "Content-type: text/html; charset=" . $encoding . " \r\n";
         $headers .= "From: " . "Teste Clínica" . " <clinica@lad566.com.br> \r\n";
         $headers .= "MIME-Version: 1.0 \r\n";
         $headers .= "Content-Transfer-Encoding: 8bit \r\n";
@@ -61,13 +62,84 @@ class ConfirmController extends Controller
                 $_SESSION['swal'] = "Conta confirmada com sucesso";
                 $_SESSION['icon'] = 'success';
             } else {
-                $_SESSION['swal'] = "Conta confirmada com sucesso";
-                $_SESSION['icon'] = 'success';
+                $_SESSION['swal'] = "Link expirado, você já está confirmado";
+                $_SESSION['icon'] = 'warning';
             }
         } else {
             $_SESSION['swal'] = "Faça login com sua conta para confirmar";
             $_SESSION['icon'] = 'warning';
         }
         $this->redirect('/');
+    }
+        private function createEmail(array $dados)
+    {
+        /*$email = '
+        <style> 
+
+        </style>
+        <p>
+        Olá, <strong>' . $dados['nome'] . '</strong>! Seja bem vindo ao nosso sistema.<br/>
+        Para prosseguir utilizando nosso sistema clique no botão abaixo.<br/>
+        </p>
+        <a class="button" href="' . $dados['link'] . '"> Confirmar </a>
+        ';*/
+        $email = '
+        <style>
+.myButton {
+	box-shadow:inset 0px 1px 0px 0px #cf866c;
+	background:linear-gradient(to bottom, #d0451b 5%, #bc3315 100%);
+	background-color:#d0451b;
+	border-radius:3px;
+	border:1px solid #942911;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:13px;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #854629;
+}
+.myButton:hover {
+	background:linear-gradient(to bottom, #bc3315 5%, #d0451b 100%);
+	background-color:#bc3315;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+</style>
+    <body style="margin: 0; padding: 0;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <td>
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="50%">
+                        <tr>
+                            <td bgcolor="#ffffff">
+                               <img src="https://prints.lad566.com.br/clinica_teste.jpg" alt="Clinica Teste" title="Clínica Teste">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                Olá <strong>' . $dados['nome'] . '</strong> Seja bem-vindo(a) ao nosso sistema.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                Clique no botão para concluir seu cadastro e seguir para a área do paciente!
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                <a class="myButton" href="' . $dados['link'] . '">Confirmar</a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </body>
+        ';
+        return $email;
     }
 }
